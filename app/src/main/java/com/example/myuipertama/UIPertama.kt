809d0d129d18fd9a3ea721d9883efef8fa6622f4
IndraHfid <indrahafidsaputra@gmail.com>
2025-10-24 14:10:51 +0700

    Tambahkan Card kedua dengan warna hijau dan data profil berbeda

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -110,7 +110,47 @@ fun AktivitasPertama(modifier: Modifier) {
             }
         }
 
-
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color(0xFF008000)
+            )
+        ) {
+            Row {
+                val gambar = painterResource(id = R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(all = 5.dp)
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.name3),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.alamat3),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.NIM),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
+                }
+            }
+        }
 
         Box(
             modifier = Modifier
